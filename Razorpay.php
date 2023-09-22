3696b28a9d546816c5109ce60bf0697c2c482ca4
Albin Varghese <albin8285@gmail.com>
2023-09-22 18:38:49 +0530

    fix: replace InjectionAwareInterface from FOSSBilling

--- Razorpay.php
+++ Razorpay.php
@@ -1,6 +1,6 @@
 <?php
 
-use Box\InjectionAwareInterface;
+use FOSSBilling\InjectionAwareInterface;
 use Razorpay\Api\Api;
 use Razorpay\Api\Errors\BadRequestError;
 use Razorpay\Api\Errors\Error;
@@ -9,9 +9,14 @@ use Razorpay\Api\Errors\ServerError;
 use Razorpay\Api\Errors\SignatureVerificationError;
 
 /**
- * Razorpay Boxbilling Integration.
+ * BoxBilling.
  *
- * @author Albin Varghese
+ * @copyright BoxBilling, Inc (https://www.boxbilling.org)
+ * @license   Apache-2.0
+ *
+ * Copyright BoxBilling, Inc
+ * This source file is subject to the Apache-2.0 License that is bundled
+ * with this source code in the file LICENSE
  */
 class Payment_Adapter_RazorPay implements InjectionAwareInterface
 {
