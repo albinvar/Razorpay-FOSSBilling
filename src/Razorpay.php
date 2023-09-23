6575597cf23d04356c1261bea1176e533d10cd8d
Albin Varghese <albin8285@gmail.com>
2023-09-23 13:19:57 +0530

    style: php-cs-fix

--- src/Razorpay.php
+++ src/Razorpay.php
@@ -9,7 +9,6 @@ use Razorpay\Api\Errors\GatewayError;
 use Razorpay\Api\Errors\ServerError;
 use Razorpay\Api\Errors\SignatureVerificationError;
 
-
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     // Development environment
     require_once __DIR__ . '/../vendor/autoload.php';
